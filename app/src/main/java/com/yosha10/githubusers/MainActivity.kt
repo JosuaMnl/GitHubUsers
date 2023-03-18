a21694bfe45b5802811ec5bf9697960112cc123c
JosuaMnl <josuamnalu@gmail.com>
2023-03-18 16:49:43 +0700

    Make UI Search View

--- app/src/main/java/com/yosha10/githubusers/MainActivity.kt
+++ app/src/main/java/com/yosha10/githubusers/MainActivity.kt
@@ -2,10 +2,30 @@ package com.yosha10.githubusers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
+import android.widget.Toast
+import com.yosha10.githubusers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.search.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                binding.search.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
     }
+
+
 }
